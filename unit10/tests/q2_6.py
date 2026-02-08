03a3de682d1a084a218097a2c6fa752e24ee7004
Tomer Libal <shaolintl@gmail.com>
2026-02-08 18:39:32 +0100

    finished the 10 units

---
+++ unit10/tests/q2_6.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2_6',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> int(SD_of_sample_means) == 3\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
